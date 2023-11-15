e9b46bbf6ebae913be722f5552efc79431a3b6ae
Petit-Homme Wadly <wadleyp96@gmail.com>
2023-11-15 15:09:37 -0500

    Animation in module no internet

--- frontend/src/pages/Departures.jsx
+++ frontend/src/pages/Departures.jsx
@@ -5,6 +5,8 @@ import Footer from '../components/footer/Footer';
 import { FaPlaneDeparture } from 'react-icons/fa';
 
 const apiUrl = 'https://flightsystem.atalou.info/php-api-fetch/src/api/allDepartures.php';
+// const apiUrl = 'http://localhost/flight_version_II/php-api-fetch/src/api/allDepartures.php';
+
 
 const Departures = () => {
   const [departuresData, setDeparturesData] = useState([]);
