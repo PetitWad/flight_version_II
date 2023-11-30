e853f21dbf285b140ea3fa7ace3faf194ff75114
Petit-Homme Wadly <79170334+PetitWad@users.noreply.github.com>
2023-11-30 13:43:13 -0500

    Delete frontend directory

--- frontend/src/config/config.js
+++
@@ -1,7 +0,0 @@
-export function getLogoUrlByAirline(airlinelogoArray, targetLogo) {
-    // Recherche un objet dans le tableau qui a le même logo que la cible.
-    const finLogo = airlinelogoArray.find(logo => logo.sigle_airline === targetLogo);
-  
-    return finLogo ? finLogo.logo_url : null;
-  }
-  
